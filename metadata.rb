0acbaa42365fba5da87f6cb5954a2f19e0b08a65
Steffen Gebert <steffen.gebert@typo3.org>
2014-08-15 10:18:33 +0200

    Use chef-server cookbook ~> 2.1.0

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          "Apache 2.0"
 description      "Chef server on chef.typo3.org"
 version          "0.1.5"
 
-depends "chef-server", "= 2.0.0"
+depends "chef-server", "~> 2.1.0"
 depends "ssl_certificates"

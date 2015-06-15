aa72afce84c1e704e51928d93a877b799eebd26e
Steffen Gebert <steffen.gebert@typo3.org>
2015-06-15 22:28:09 +0200

    Update chef server version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # this requires Omnitrack to work, which however always fails with "uninitialized constant Chef::Recipe::OmnitruckClient"
 # node.override['chef-server']['version'] = '11.8.0'
 
-node.override['chef-server']['package_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/lucid/chef-server_11.1.5-1_amd64.deb'
+node.override['chef-server']['package_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/lucid/chef-server_11.1.7-1_amd64.deb'
 
 node.override['chef-server']['api_fqdn'] = "chef.typo3.org"
 node.override['chef-server']['configuration']['nginx']['ssl_certificate'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.crt"

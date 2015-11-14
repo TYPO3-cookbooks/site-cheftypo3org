c10f601a625ddc8e2f077710693bdc5e80ff63e8
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-14 18:03:18 +0100

    Install a Berkshelf API server

---
+++ recipes/berks-api.rb
@@ -0,0 +1,23 @@
+# This installs the berks-api endpoint
+
+include_recipe "berkshelf-api-server::app"
+
+# We copy over the client.pem into a place accessible for berks-api
+template File.join(node['berkshelf_api']['home'], 'client.pem') do
+  source "/etc/chef/client.pem"
+  local true
+  owner node['berkshelf_api']['owner']
+  group node['berkshelf_api']['group']
+  notifies :restart, "runit_service[berks-api]"
+end
+
+ruby_block "ignore runit sv directories" do
+  block do
+    ignore_dir = "sv/*/log/main/*"
+    fe = Chef::Util::FileEdit.new("/etc/.gitignore")
+    fe.insert_line_if_no_match(Regexp.new(Regexp.escape(ignore_dir)), ignore_dir)
+    fe.write_file
+  end
+  only_if { File.exists?("/etc/.gitignore") }
+end
+

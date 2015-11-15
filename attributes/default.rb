785b2a0f57bbcdb100a3edc570e8dbf5bb34f321
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-15 18:00:52 +0100

    Draft attribute for version

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@
 default['chef-server']['package_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/lucid/chef-server_11.1.7-1_amd64.deb'
 
 default['chef-server']['api_fqdn'] = "chef.typo3.org"
+# default['chef-server']['version'] = "12.3.0"
 # default['chef-server']['configuration']['nginx']['ssl_certificate'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.crt"
 # default['chef-server']['configuration']['nginx']['ssl_certificate_key'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.key"
 

e523a71eeaf7f9ceadb3b9c6f23dc98fb79650c9
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-15 18:01:37 +0100

    Work around Debian 8 systemd setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,14 @@
 #   ca_bundle_combined true
 # end
 
+package "apt-transport-https"
+
+if node['platform'] == 'debian' && node['lsb']['codename'] == 'jessie'
+  link "/usr/lib/systemd/system" do
+    to "/etc/systemd/system"
+  end
+end
+
 include_recipe "chef-server::default"
 
 include_recipe "#{cookbook_name}::berks-api"

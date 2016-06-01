5d573ad6fc41a35ef38216a97ecfb1deed198bf1
Steffen Gebert <steffen.gebert@typo3.org>
2016-06-01 22:10:36 +0200

    [TASK] Update chef server source to bintray

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # this requires Omnitrack to work, which however always fails with "uninitialized constant Chef::Recipe::OmnitruckClient"
 # node.override['chef-server']['version'] = '11.8.0'
 
-default['chef-server']['package_file'] = 'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/lucid/chef-server_11.1.7-1_amd64.deb'
+default['chef-server']['package_file'] = 'https://packages.chef.io/stable/ubuntu/10.04/chef-server_11.1.7-1_amd64.deb'
 
 default['chef-server']['api_fqdn'] = "chef.typo3.org"
 default['chef-server']['configuration']['nginx']['ssl_certificate'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.crt"

da7a897abbf108fcdd0469c232d2bccd5c06d078
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-23 09:27:29 +0100

    [BUGFIX] Increase number and timeouts for depsolver.
    We get
    
    > unable to solve dependencies in alotted time
    
    way too often.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,4 +7,10 @@ default['chef-server']['api_fqdn'] = "chef.typo3.org"
 default['chef-server']['configuration']['nginx']['ssl_certificate'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.crt"
 default['chef-server']['configuration']['nginx']['ssl_certificate_key'] = "#{node[:ssl_certificates][:path]}/wildcard.typo3.org.key"
 
+#<> Use more workers for dependency resolution
+default['chef-server']['configuration']['erchef']['depsolver_worker_count'] = 20
+#<> Give workers more time for dependency resolution (30sec)
+default['chef-server']['configuration']['erchef']['depsolver_timeout'] = 30000
+
+
 default['site-cheftypo3org']['ssl_certificate'] = "wildcard.typo3.org"
